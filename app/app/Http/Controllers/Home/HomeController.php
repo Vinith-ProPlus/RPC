fb6992bc138f4c086e191cf5f59ce6bb8f5209aa
Vinith Kumar <vinithkumarpropluslogics@gmail.com>
2024-03-29 13:08:17 +0530

    dflag banners rejected

--- app/app/Http/Controllers/Home/HomeController.php
+++ app/app/Http/Controllers/Home/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
     public function GuestView(Request $req)
     {
         $FormData['Company'] = DB::table('tbl_company_settings')->select('KeyName', 'KeyValue')->get()->pluck('KeyValue', 'KeyName')->toArray();
-        $FormData['Banners'] = DB::Table('tbl_banner_images')->where('BannerType', 'Web')
+        $FormData['Banners'] = DB::Table('tbl_banner_images')->where('BannerType', 'Web')->where('DFlag', 0)
             ->select('BannerTitle', 'BannerType', DB::raw('CONCAT("' . url('/') . '/", BannerImage) AS BannerImage'))->get();
         $PCatagories = DB::Table('tbl_product_category')->where('ActiveStatus', 'Active')->where('DFlag', 0)->select('PCName', 'PCID', 'PCImage')
             ->inRandomOrder()->take(10)->get();
